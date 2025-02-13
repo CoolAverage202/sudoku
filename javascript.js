369a6c51f477b80d59f5d5d46099c96bd358dea9
Cristopher Perez <c.perez86@yahoo.com>
2025-02-12 22:55:31 -0800

    Creating the boiler plate

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < 9; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 9; j++) {
+        let cell = document.createElement("input");
+        cell.type = "text";
+        cell.classList.add("cell");
+        cell.id = `cell-${i * 9 + j}`;
+        row.appendChild(cell);
+    }
+
+    grid.appendChild(row);
+}
